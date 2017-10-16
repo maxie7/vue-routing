b26fcb832eec725296300e0a64dea9d5cf063873
Kos Max <maxie7@protonmail.com>
2017-10-16 23:14:36 +0200

    Add nested routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
     <h1>The User Page</h1>
-    <br>
-    <p>Loaded ID: {{ id }}</p>
+    <hr>
     <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        id: this.$route.params.id
-      }
-    },
-    watch: {
-      '$route' (to, from) {
-        this.id = to.params.id;
-      }
-    },
     methods: {
       navigateToHome() {
         this.$router.push('/');
